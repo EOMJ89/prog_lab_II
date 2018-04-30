c6f6d945a9ac648647acc989e300fde412563182
Emilio Segado <emiliosegado14@gmail.com>
2018-04-30 18:34:57 -0300

    Added Projects for Exam Practices

---
+++ ClasesAnimales/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using ClasesAnimales;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grupo veterinario = new Grupo("Río Unica", ETipoManada.Mixta);
+            Perro moro = new Perro("Moro", "Pitbull");
+            Perro julio = new Perro("Julio", "Cruza",13,false);
+            Perro ramon = new Perro("Ramón", "Salchicha", 2,true);
+            Gato jose = new Gato("José", "Angora");
+            Gato hernan = new Gato("Hernán", "Cruza");
+            Gato fer = new Gato("Hernán", "Siames");
+
+            veterinario += moro;
+            veterinario += julio;
+            veterinario += ramon;
+            veterinario += jose;
+            veterinario += hernan;
+            veterinario += fer;
+
+            Console.WriteLine((string)veterinario);
+            Console.ReadLine();
+        }
+    }
+}
