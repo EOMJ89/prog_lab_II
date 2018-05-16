632674c7d94e10a20e4d13979c1fb81767de5f91
Emilio Segado <emiliosegado14@gmail.com>
2018-05-16 20:12:47 -0300

    Add files via upload

---
+++ Segado.Emilio_Parte3/Ejercicio47-Ejecutable/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ejercicio47_Library;
+
+namespace Ejercicio47_Ejecutable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Factura f1 = new Factura(3);
+            Factura f2 = new Factura(5);
+            Factura f3 = new Factura(2);
+
+            Recibo r1 = new Recibo();
+            Recibo r2 = new Recibo(2);
+            Recibo r3 = new Recibo();
+
+            //Compilan perfectamente
+            Contabilidad<Factura, Recibo> contaduria = new Contabilidad<Factura, Recibo>();
+            Contabilidad<Documento, Recibo> contaduria2 = new Contabilidad<Documento, Recibo>();
+            /* 
+             * Contabilidad<Recibo,Factura> contaduria = new Contabilidad<Recibo,Factura>();
+             * Contabilidad<Factura, Documento> contaduria = new Contabilidad<Factura, Documento>();
+             * //Esto no compilaria porque Factura no tiene un constructor Default
+            */
+
+            contaduria += f1;
+            contaduria += r1;
+            contaduria += f2;
+            contaduria += r2;
+            contaduria += f3;
+            contaduria += r3;
+
+            contaduria2 += f1;
+            contaduria2 += r1;
+            contaduria2 += f2;
+            contaduria2 += r2;
+            contaduria2 += f3;
+            contaduria2 += r3;
+
+            Console.WriteLine(contaduria.ToString("Factura", "Recibo"));
+
+            Console.WriteLine(contaduria2.ToString("Documento", "Recibo"));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Segado.Emilio_Parte3/Ejercicio47-Library/Class1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio47_Library
+{
+    public class Documento
+    {
+        protected int _numero;
+
+        public Documento(int numero)
+        { this._numero = numero; }
+
+        public override string ToString()
+        {
+            return this._numero.ToString();
+        }
+    }
+
+    public class Recibo : Documento
+    {
+        public Recibo() : this(0) { }
+
+        public Recibo(int numero) : base(numero) { }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class Factura : Documento
+    {
+        public Factura(int numero) : base(numero) { }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class Contabilidad<T, U>
+        where T : Documento
+        where U : Documento, new() //Restricciones multiples requieren usar ","
+    {
+        private List<T> _egresos;
+        private List<U> _ingresos;
+
+        public Contabilidad()
+        {
+            this._egresos = new List<T>();
+            this._ingresos = new List<U>();
+        }
+
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, T egreso)
+        {
+            Contabilidad<T, U> aux = c;//No requiere validar porque la unica otra chance ya está en sobrecargas
+            aux._egresos.Add(egreso);
+
+            return c;
+        }
+
+        public static Contabilidad<T, U> operator +(Contabilidad<T, U> c, U ingreso)
+        {
+            Contabilidad<T, U> aux = c; //No requiere validar porque la unica otra chance ya está en sobrecargas
+            aux._ingresos.Add(ingreso);
+
+            return aux;
+        }
+
+        public string ToString(string tText, string uText)
+        {
+            StringBuilder stringBuild = new StringBuilder();
+
+            stringBuild.AppendFormat("Lista de {0}:\n", tText);
+            foreach (T tAux in this._egresos)
+            { stringBuild.AppendLine(tAux.ToString()); }
+
+            stringBuild.AppendFormat("Lista de {0}:\n", uText);
+            foreach (U uAux in this._ingresos)
+            { stringBuild.AppendLine(uAux.ToString()); }
+
+            return stringBuild.ToString();
+        }
+    }
+}
